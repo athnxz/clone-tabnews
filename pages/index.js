1494ee6f0df9b50ea92c9649c23d7771280821bd
Athos Gabriel <149092561+athnxz@users.noreply.github.com>
2025-01-18 14:26:16 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Higo, eu amo você e se você me ama, dá um risadinha!</h1>
+    return <h1>Higo, eu amo você e se você me ama, dá um risadinha! :3 </h1>
 }
 
 export default Home;
